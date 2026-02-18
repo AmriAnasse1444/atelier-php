21fd7d4fff79e7bf9011685bde925c5795edae12
AmriAnasse1444 <amrianasse550@gmail.com>
2026-02-18 23:03:53 +0000

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang ="fr">
+    <head>
+        <meta charset= "utf-8">
+        <title> page dynamique</title>
+    </head>
+    <body>
+        <?php 
+        $nom = "Étudiant";
+        echo "<h1>Bonjour, $nom !</h1>";
+        ?>
+        <p>Ceci est une page générer en combinant PHP et HTML.</p>
+    </body>
+</html>
+        
